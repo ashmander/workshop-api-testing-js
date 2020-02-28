615e31c4cdf8b7086a73d92963be8d13e7f4aac6
Andrés Felipe González <37818322+ashmander@users.noreply.github.com>
2020-02-28 07:09:21 -0500

    Integration (#10)
    * integration with travis
    
    * mochawesome
    
    * add eof
    
    * eslinter
    
    * add eof
    
    * add eof

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,12 +2,12 @@ const agent = require('superagent');
 const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
 describe('First Api Tests', () => {
   it('Consume GET Service', async () => {
     const response = await agent.get('https://httpbin.org/ip');
-  
+
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body).to.have.property('origin');
   });
@@ -18,23 +18,23 @@ describe('First Api Tests', () => {
       age: '31',
       city: 'New York'
     };
-  
+
     const response = await agent.get('https://httpbin.org/get').query(query);
-  
+
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body.args).to.eql(query);
   });
 
-  it ('Consume HEAD Service', async () => {
+  it('Consume HEAD Service', async () => {
     const response = await agent.head('https://httpbin.org/get');
 
     expect(response.status).to.equals(statusCode.OK);
     expect(response.header).to.have.property('server');
   });
 
-  it ('Consume PUT Service', async () => {
+  it('Consume PUT Service', async () => {
     const response = await agent.put('https://httpbin.org/put');
-    
+
     expect(response.status).to.equals(statusCode.OK);
     expect(response.body).have.property('origin');
   });
