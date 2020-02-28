9d9c41b14127ca10dea161ac6c22c23f9bb62602
Andres Gonzalez <allwin123456@hotmail.es>
2020-02-28 08:29:23 -0500

    autentication

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -12,14 +12,13 @@ describe('Github Api Test', () => {
       const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
         .auth('token', process.env.ACCESS_TOKEN)
         .set('User-Agent', 'agent');
-
       expect(response.status).to.equal(statusCode.OK);
       expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
     });
   });
-  it('Via OAuth2 Tokens by parameter', () =>
-  agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+  it('Via OAuth2 Tokens by parameter', () => agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
     .query(`access_token=${process.env.ACCESS_TOKEN}`)
+    .set('User-Agent', 'agent')
     .then((response) => {
       expect(response.status).to.equal(statusCode.OK);
       expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
