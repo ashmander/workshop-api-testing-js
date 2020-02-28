fa5e097360679aab3776aa0c18c1ccd64d45cd84
Andres Gonzalez <allwin123456@hotmail.es>
2020-02-28 08:36:55 -0500

    ACCESS_TOKEN as a const

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -5,19 +5,20 @@ const { expect } = require('chai');
 const urlBase = 'https://api.github.com';
 const githubUserName = 'ashmander';
 const repository = 'workshop-api-testing-js';
+const token = proces.env.ACCESS_TOKEN;
 
 describe('Github Api Test', () => {
   describe('Authentication', () => {
     it('Via OAuth2 Tokens by Header', async () => {
       const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
-        .auth('token', process.env.ACCESS_TOKEN)
+        .auth('token', token)
         .set('User-Agent', 'agent');
       expect(response.status).to.equal(statusCode.OK);
       expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
     });
   });
   it('Via OAuth2 Tokens by parameter', () => agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
-    .query(`access_token=${process.env.ACCESS_TOKEN}`)
+    .query(`access_token=${token}`)
     .set('User-Agent', 'agent')
     .then((response) => {
       expect(response.status).to.equal(statusCode.OK);
